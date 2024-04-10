1733b20f9a5aa2c6392b7dfcf31b3091ade7ce0e
Alex May <amay@acvauctions.com>
2024-04-10 15:16:38 -0400

    session 3 additions

--- src/components/AvatarDialog.vue
+++ src/components/AvatarDialog.vue
@@ -2,8 +2,8 @@
   <v-dialog max-width="600" max-height="700">
 
     <template v-slot:activator="{ props }">
-          <v-avatar v-if="mdAndUp" v-bind="props" size="150" style="float: left; margin-right: 10px;" rounded  :image="n.raw.avatar"></v-avatar>
-          <v-avatar v-if="smAndDown" v-bind="props" size="85" style="float: left; margin-right: 15px;" rounded :image="n.raw.avatar"></v-avatar>
+          <v-avatar v-if="mdAndUp" v-bind="props" size="150" style="float: left; margin-right: 10px;" rounded  :image="n.raw.thumb ?? n.raw.avatar"></v-avatar>
+          <v-avatar v-if="smAndDown" v-bind="props" size="85" style="float: left; margin-right: 15px;" rounded :image="n.raw.thumb ?? n.raw.avatar"></v-avatar>
     </template>
 
     <template v-slot:default="{ isActive }">
